d7b362ac898193ed12ea9023a03de7e7778a9a42
yurabogdanov86 <yurabogdanov86@gmail.com>
2025-02-16 00:14:28 +0500

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 import requests
 
 def send_message():
-    token = "${{ secrets.TELEGRAM_TOKEN }}"
-    chat_id = "${{ secrets.TELEGRAM_CHANNEL_ID }}"
+    token = "7229132515:AAHx9mho6IQn2LnseRtVxT_3Z6h0UfeeG6E"  # Замените на ваш токен
+    chat_id = "-1002424946552"  # Замените на ваш канал
     message = "Привет! Этот скрипт отправлен через GitHub Actions."
 
     url = f"https://api.telegram.org/bot{token}/sendMessage"
